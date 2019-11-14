1d1fe911ae30d85348acfb8a8aaef174d8a0ed0b
theesmoxDEV <56123279+theesmoxDEV@users.noreply.github.com>
2019-11-14 07:57:43 +0100

    Update RomanToInteger.py
    Added comments.

--- RomanToInteger.py
+++ RomanToInteger.py
@@ -1,12 +1,15 @@
-if __name__=="__main__":
+if __name__=="__main__": # This make it possible to run "python RomanToInteger.py" directly in the terminal
 
-    while True:
+    while True: # A infinite loop, so you can repeat the program untill you press "ctrl + c"
 	
-        x = str(input("Roman number to convert: "))
+        x = str(input("Roman number to convert: ")) # The input is the roman number. The function suposses that te number is correct.
 
         def romanToInt(s: str) -> int:
+		
+	    """Converts a roman number into decimal."""
+	
             total = 0
-            dict ={
+            dict ={                              # Values of each number
                 "I" : 1,
                 "V" : 5,
                 "X" : 10,
@@ -15,13 +18,17 @@ if __name__=="__main__":
                 "D" : 500,
                 "M" : 1000
             }
-            for j in range(len(s)):
-                if j == len(s)-1:
+            for j in range(len(s)):              # We loop over the number indices.
+			
+                if j == len(s)-1:                # If the number is at the end, we just add it to the total.
                     total += dict[s[j]]
-                elif dict[s[j]] < dict[s[j+1]]:
+
+		elif dict[s[j]] < dict[s[j+1]]:  # If the number is less than the next number, we substract it from the total.
                     total -= dict[s[j-1]]
-                else:
-                    total += dict[s[j]]
-            return total
+                
+		else:
+                    total += dict[s[j]]          # If not at the end and neither less than the next number, we just add it to the total. 
+            
+	    return total
 
         print(romanToInt(x.upper()))
